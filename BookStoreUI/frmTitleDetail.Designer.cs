aedddbf51dbb0ef0b844737bae480ba3755976f4
Dante54321 <davidalejmartinez@gmail.com>
2025-11-19 20:55:40 -0500

    Refactor control initialization and layout in frmTitleDetail
    Reordered initialization of controls and updated their layout positions in frmTitleDetail.Designer.cs for improved clarity and maintainability. Adjusted control properties and table layout sizes to better organize the form's UI elements.

--- BookStoreUI/frmTitleDetail.Designer.cs
+++ BookStoreUI/frmTitleDetail.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             tlpFields = new TableLayoutPanel();
+            txtNotes = new TextBox();
+            nudYtdSales = new NumericUpDown();
+            nudRoyalty = new NumericUpDown();
+            nudAdvance = new NumericUpDown();
+            nudPrice = new NumericUpDown();
             txtPubId = new TextBox();
             txtTitle = new TextBox();
             lblTitleid = new Label();
@@ -39,23 +44,18 @@
             lblAdvance = new Label();
             lblRoyalty = new Label();
             lblYtd_sales = new Label();
-            lblNotes = new Label();
             lblPubdate = new Label();
             txtTitle_id = new TextBox();
             comboBox1 = new ComboBox();
-            nudPrice = new NumericUpDown();
-            nudAdvance = new NumericUpDown();
-            nudRoyalty = new NumericUpDown();
-            nudYtdSales = new NumericUpDown();
-            txtNotes = new TextBox();
+            lblNotes = new Label();
             dtp = new DateTimePicker();
             btnSave = new Button();
             btnCancel = new Button();
             tlpFields.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)nudPrice).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)nudAdvance).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)nudRoyalty).BeginInit();
             ((System.ComponentModel.ISupportInitialize)nudYtdSales).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudRoyalty).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudAdvance).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudPrice).BeginInit();
             SuspendLayout();
             // 
             // tlpFields
@@ -99,14 +99,70 @@
             tlpFields.RowStyles.Add(new RowStyle(SizeType.Absolute, 80F));
             tlpFields.RowStyles.Add(new RowStyle(SizeType.Percent, 11.1111107F));
             tlpFields.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tlpFields.Size = new Size(800, 584);
+            tlpFields.Size = new Size(800, 431);
             tlpFields.TabIndex = 8;
+            // 
+            // txtNotes
+            // 
+            txtNotes.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            txtNotes.CharacterCasing = CharacterCasing.Upper;
+            txtNotes.Location = new Point(175, 307);
+            txtNotes.MaxLength = 4;
+            txtNotes.Multiline = true;
+            txtNotes.Name = "txtNotes";
+            txtNotes.ScrollBars = ScrollBars.Vertical;
+            txtNotes.Size = new Size(622, 74);
+            txtNotes.TabIndex = 25;
+            // 
+            // nudYtdSales
+            // 
+            nudYtdSales.Anchor = AnchorStyles.Left;
+            nudYtdSales.Increment = new decimal(new int[] { 100, 0, 0, 0 });
+            nudYtdSales.Location = new Point(175, 269);
+            nudYtdSales.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            nudYtdSales.Name = "nudYtdSales";
+            nudYtdSales.Size = new Size(180, 31);
+            nudYtdSales.TabIndex = 24;
+            nudYtdSales.ThousandsSeparator = true;
+            // 
+            // nudRoyalty
+            // 
+            nudRoyalty.Anchor = AnchorStyles.Left;
+            nudRoyalty.Increment = new decimal(new int[] { 100, 0, 0, 0 });
+            nudRoyalty.Location = new Point(175, 231);
+            nudRoyalty.Name = "nudRoyalty";
+            nudRoyalty.Size = new Size(180, 31);
+            nudRoyalty.TabIndex = 23;
+            nudRoyalty.ThousandsSeparator = true;
+            // 
+            // nudAdvance
+            // 
+            nudAdvance.Anchor = AnchorStyles.Left;
+            nudAdvance.DecimalPlaces = 2;
+            nudAdvance.Increment = new decimal(new int[] { 100, 0, 0, 0 });
+            nudAdvance.Location = new Point(175, 193);
+            nudAdvance.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
+            nudAdvance.Name = "nudAdvance";
+            nudAdvance.Size = new Size(180, 31);
+            nudAdvance.TabIndex = 22;
+            nudAdvance.ThousandsSeparator = true;
+            // 
+            // nudPrice
+            // 
+            nudPrice.Anchor = AnchorStyles.Left;
+            nudPrice.DecimalPlaces = 2;
+            nudPrice.Location = new Point(175, 155);
+            nudPrice.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
+            nudPrice.Name = "nudPrice";
+            nudPrice.Size = new Size(180, 31);
+            nudPrice.TabIndex = 21;
+            nudPrice.ThousandsSeparator = true;
             // 
             // txtPubId
             // 
             txtPubId.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             txtPubId.CharacterCasing = CharacterCasing.Upper;
-            txtPubId.Location = new Point(175, 180);
+            txtPubId.Location = new Point(175, 117);
             txtPubId.MaxLength = 4;
             txtPubId.Name = "txtPubId";
             txtPubId.Size = new Size(622, 31);
@@ -115,7 +171,7 @@
             // txtTitle
             // 
             txtTitle.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtTitle.Location = new Point(175, 68);
+            txtTitle.Location = new Point(175, 41);
             txtTitle.MaxLength = 80;
             txtTitle.Name = "txtTitle";
             txtTitle.Size = new Size(622, 31);
@@ -133,7 +189,7 @@
             // lbltitle
             // 
             lbltitle.AutoSize = true;
-            lbltitle.Location = new Point(3, 56);
+            lbltitle.Location = new Point(3, 38);
             lbltitle.Name = "lbltitle";
             lbltitle.Size = new Size(41, 25);
             lbltitle.TabIndex = 1;
@@ -142,7 +198,7 @@
             // lbltype
             // 
             lbltype.AutoSize = true;
-            lbltype.Location = new Point(3, 112);
+            lbltype.Location = new Point(3, 76);
             lbltype.Name = "lbltype";
             lbltype.Size = new Size(47, 25);
             lbltype.TabIndex = 2;
@@ -151,7 +207,7 @@
             // lblPub_id
             // 
             lblPub_id.AutoSize = true;
-            lblPub_id.Location = new Point(3, 168);
+            lblPub_id.Location = new Point(3, 114);
             lblPub_id.Name = "lblPub_id";
             lblPub_id.Size = new Size(66, 25);
             lblPub_id.TabIndex = 3;
@@ -160,7 +216,7 @@
             // lblPrice
             // 
             lblPrice.AutoSize = true;
-            lblPrice.Location = new Point(3, 224);
+            lblPrice.Location = new Point(3, 152);
             lblPrice.Name = "lblPrice";
             lblPrice.Size = new Size(50, 25);
             lblPrice.TabIndex = 4;
@@ -169,7 +225,7 @@
             // lblAdvance
             // 
             lblAdvance.AutoSize = true;
-            lblAdvance.Location = new Point(3, 280);
+            lblAdvance.Location = new Point(3, 190);
             lblAdvance.Name = "lblAdvance";
             lblAdvance.Size = new Size(77, 25);
             lblAdvance.TabIndex = 5;
@@ -178,7 +234,7 @@
             // lblRoyalty
             // 
             lblRoyalty.AutoSize = true;
-            lblRoyalty.Location = new Point(3, 336);
+            lblRoyalty.Location = new Point(3, 228);
             lblRoyalty.Name = "lblRoyalty";
             lblRoyalty.Size = new Size(66, 25);
             lblRoyalty.TabIndex = 6;
@@ -187,25 +243,16 @@
             // lblYtd_sales
             // 
             lblYtd_sales.AutoSize = true;
-            lblYtd_sales.Location = new Point(3, 392);
+            lblYtd_sales.Location = new Point(3, 266);
             lblYtd_sales.Name = "lblYtd_sales";
             lblYtd_sales.Size = new Size(83, 25);
             lblYtd_sales.TabIndex = 7;
             lblYtd_sales.Text = "ytd_sales";
             // 
-            // lblNotes
-            // 
-            lblNotes.AutoSize = true;
-            lblNotes.Location = new Point(3, 448);
-            lblNotes.Name = "lblNotes";
-            lblNotes.Size = new Size(56, 25);
-            lblNotes.TabIndex = 8;
-            lblNotes.Text = "notes";
-            // 
             // lblPubdate
             // 
             lblPubdate.AutoSize = true;
-            lblPubdate.Location = new Point(3, 528);
+            lblPubdate.Location = new Point(3, 384);
             lblPubdate.Name = "lblPubdate";
             lblPubdate.Size = new Size(79, 25);
             lblPubdate.TabIndex = 9;
@@ -214,7 +261,7 @@
             // txtTitle_id
             // 
             txtTitle_id.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtTitle_id.Location = new Point(175, 12);
+            txtTitle_id.Location = new Point(175, 3);
             txtTitle_id.MaxLength = 6;
             txtTitle_id.Name = "txtTitle_id";
             txtTitle_id.Size = new Size(622, 31);
@@ -226,73 +273,26 @@
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Technical", "Business", "Psychology", "Popular_Comp", "UNDECIDED" });
-            comboBox1.Location = new Point(175, 115);
+            comboBox1.Location = new Point(175, 79);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(622, 33);
             comboBox1.TabIndex = 20;
             // 
-            // nudPrice
+            // lblNotes
             // 
-            nudPrice.Anchor = AnchorStyles.Left;
-            nudPrice.DecimalPlaces = 2;
-            nudPrice.Location = new Point(175, 229);
-            nudPrice.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
-            nudPrice.Name = "nudPrice";
-            nudPrice.Size = new Size(180, 31);
-            nudPrice.TabIndex = 21;
-            nudPrice.ThousandsSeparator = true;
-            // 
-            // nudAdvance
-            // 
-            nudAdvance.Anchor = AnchorStyles.Left;
-            nudAdvance.DecimalPlaces = 2;
-            nudAdvance.Increment = new decimal(new int[] { 100, 0, 0, 0 });
-            nudAdvance.Location = new Point(175, 292);
-            nudAdvance.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
-            nudAdvance.Name = "nudAdvance";
-            nudAdvance.Size = new Size(180, 31);
-            nudAdvance.TabIndex = 22;
-            nudAdvance.ThousandsSeparator = true;
-            // 
-            // nudRoyalty
-            // 
-            nudRoyalty.Anchor = AnchorStyles.Left;
-            nudRoyalty.Increment = new decimal(new int[] { 100, 0, 0, 0 });
-            nudRoyalty.Location = new Point(175, 348);
-            nudRoyalty.Name = "nudRoyalty";
-            nudRoyalty.Size = new Size(180, 31);
-            nudRoyalty.TabIndex = 23;
-            nudRoyalty.ThousandsSeparator = true;
-            // 
-            // nudYtdSales
-            // 
-            nudYtdSales.Anchor = AnchorStyles.Left;
-            nudYtdSales.Increment = new decimal(new int[] { 100, 0, 0, 0 });
-            nudYtdSales.Location = new Point(175, 404);
-            nudYtdSales.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
-            nudYtdSales.Name = "nudYtdSales";
-            nudYtdSales.Size = new Size(180, 31);
-            nudYtdSales.TabIndex = 24;
-            nudYtdSales.ThousandsSeparator = true;
-            // 
-            // txtNotes
-            // 
-            txtNotes.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            txtNotes.CharacterCasing = CharacterCasing.Upper;
-            txtNotes.Location = new Point(175, 451);
-            txtNotes.MaxLength = 4;
-            txtNotes.Multiline = true;
-            txtNotes.Name = "txtNotes";
-            txtNotes.ScrollBars = ScrollBars.Vertical;
-            txtNotes.Size = new Size(622, 74);
-            txtNotes.TabIndex = 25;
+            lblNotes.AutoSize = true;
+            lblNotes.Location = new Point(3, 304);
+            lblNotes.Name = "lblNotes";
+            lblNotes.Size = new Size(56, 25);
+            lblNotes.TabIndex = 8;
+            lblNotes.Text = "notes";
             // 
             // dtp
             // 
             dtp.Anchor = AnchorStyles.Left;
             dtp.CustomFormat = "yyyy/MM/dd";
             dtp.Format = DateTimePickerFormat.Custom;
-            dtp.Location = new Point(175, 533);
+            dtp.Location = new Point(175, 392);
             dtp.MaxDate = new DateTime(2025, 11, 19, 0, 0, 0, 0);
             dtp.Name = "dtp";
             dtp.Size = new Size(300, 31);
@@ -339,10 +339,10 @@
             Text = "Add/Edit Title";
             tlpFields.ResumeLayout(false);
             tlpFields.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)nudPrice).EndInit();
-            ((System.ComponentModel.ISupportInitialize)nudAdvance).EndInit();
-            ((System.ComponentModel.ISupportInitialize)nudRoyalty).EndInit();
             ((System.ComponentModel.ISupportInitialize)nudYtdSales).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudRoyalty).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudAdvance).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudPrice).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
